31d929db2f2ff8deecdc4f54ae0fff63edccdf09
Stella Cannefax <stella@mystenlabs.com>
2022-09-15 10:55:24 -0500

    indent SuiObjectData

--- src/SuiDotNet.Client/Requests/SuiObjectData.cs
+++ src/SuiDotNet.Client/Requests/SuiObjectData.cs
@@ -3,23 +3,23 @@ using Newtonsoft.Json;
 
 namespace SuiDotNet.Client.Requests
 {
-public class SuiObjectData
-{
-    [JsonProperty("dataType")]
-    public string DataType { get; }
-    [JsonProperty("type")]
-    public string Type { get; }
-    [JsonProperty("has_public_transfer")]
-    public bool HasPublicTransfer { get; }
-    [JsonProperty("fields")]
-    public Dictionary<string, object> Fields { get; }
-
-    public SuiObjectData(string dataType, string type, bool hasPublicTransfer, Dictionary<string, object> fields)
+    public class SuiObjectData
     {
-        DataType = dataType;
-        Type = type;
-        HasPublicTransfer = hasPublicTransfer;
-        Fields = fields;
+        [JsonProperty("dataType")]
+        public string DataType { get; }
+        [JsonProperty("type")]
+        public string Type { get; }
+        [JsonProperty("has_public_transfer")]
+        public bool HasPublicTransfer { get; }
+        [JsonProperty("fields")]
+        public Dictionary<string, object> Fields { get; }
+
+        public SuiObjectData(string dataType, string type, bool hasPublicTransfer, Dictionary<string, object> fields)
+        {
+            DataType = dataType;
+            Type = type;
+            HasPublicTransfer = hasPublicTransfer;
+            Fields = fields;
+        }
     }
-}
 }
