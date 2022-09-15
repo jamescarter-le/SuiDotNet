4f39c5091f0ec5a6f0bf78ea20ef4c132a6231c3
Stella Cannefax <stella@mystenlabs.com>
2022-09-15 10:49:38 -0500

    indent into namespaces

--- src/SuiDotNet.Client/Converters/ObjectOwnerConverter.cs
+++ src/SuiDotNet.Client/Converters/ObjectOwnerConverter.cs
@@ -5,42 +5,40 @@ using SuiDotNet.Client.Requests;
 
 namespace SuiDotNet.Client.Converters
 {
-
-public class ObjectOwnerConverter : JsonConverter<ObjectOwner>
-{
-    public override bool CanWrite
+    public class ObjectOwnerConverter : JsonConverter<ObjectOwner>
     {
-        get { return false; }
-    }
-
-    public override void WriteJson(JsonWriter writer, ObjectOwner value, JsonSerializer serializer)
-    {
-    }
-
-    public override ObjectOwner ReadJson(JsonReader reader, Type objectType, ObjectOwner existingValue, bool hasExistingValue, JsonSerializer serializer)
-    {
-        if (reader.TokenType == JsonToken.String)
+        public override bool CanWrite
         {
-            if (reader.Value.ToString() == "Shared")
-            {
-                return new ObjectOwner(OwnerType.Shared);
-            }
-        }
-        else
-        {
-            var jObject = serializer.Deserialize<JObject>(reader);
-            if (jObject.ContainsKey("AddressOwner"))
-            {
-                return new ObjectOwner(OwnerType.Address, jObject["AddressOwner"].Value<string>());
-            }
-            else if (jObject.ContainsKey("ObjectOwner"))
-            {
-                return new ObjectOwner(OwnerType.Object, jObject["AddressOwner"].Value<string>());
-            }
+            get { return false; }
         }
 
-        return new ObjectOwner(OwnerType.Unknown);
-    }
-}
+        public override void WriteJson(JsonWriter writer, ObjectOwner value, JsonSerializer serializer)
+        {
+        }
 
+        public override ObjectOwner ReadJson(JsonReader reader, Type objectType, ObjectOwner existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+            {
+                if (reader.Value.ToString() == "Shared")
+                {
+                    return new ObjectOwner(OwnerType.Shared);
+                }
+            }
+            else
+            {
+                var jObject = serializer.Deserialize<JObject>(reader);
+                if (jObject.ContainsKey("AddressOwner"))
+                {
+                    return new ObjectOwner(OwnerType.Address, jObject["AddressOwner"].Value<string>());
+                }
+                else if (jObject.ContainsKey("ObjectOwner"))
+                {
+                    return new ObjectOwner(OwnerType.Object, jObject["AddressOwner"].Value<string>());
+                }
+            }
+
+            return new ObjectOwner(OwnerType.Unknown);
+        }
+    }
 }
--- src/SuiDotNet.Client/Requests/ObjectDataResponse.cs
+++ src/SuiDotNet.Client/Requests/ObjectDataResponse.cs
@@ -2,11 +2,11 @@
 
 namespace SuiDotNet.Client.Requests
 {
-public class ObjectDataResponse
-{
-    [JsonProperty("status")]
-    public SuiObjectStatus Status { get; set; }
-    [JsonProperty("details")] // Need to set JsonConverter to ignore deserializing this if it is a string.
-    public SuiObject? Details { get; set; }
-}
+    public class ObjectDataResponse
+    {
+        [JsonProperty("status")]
+        public SuiObjectStatus Status { get; set; }
+        [JsonProperty("details")] // Need to set JsonConverter to ignore deserializing this if it is a string.
+        public SuiObject? Details { get; set; }
+    }
 }
